1a7ea63d112c2707bc1f1dffd06e053aaa0f1d5b
Lnvictor <victorhpereira26@gmail.com>
2023-03-04 15:11:25 -0300

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+const robotron = document.querySelector(".robotron");
+robotron.addEventListener("click", () => {
+    console.log('Bem vindo ao robotron 2000');
+});
+
+function dizOi(){
+    console.log('oi');
+}
